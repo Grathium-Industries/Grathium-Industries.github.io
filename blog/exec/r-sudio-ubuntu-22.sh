b192764f6d3d8bb01c9fff22d10a48b2739c4138
Grathium <grath@duck.com>
2022-07-18 19:03:07 +1000

    stylalistic upgrades

--- blog/exec/r-sudio-ubuntu-22.sh
+++ blog/exec/r-sudio-ubuntu-22.sh
@@ -1,5 +1,16 @@
+# uninstall old instance of rstudio
+sudo gdebi -r rstudio
+
+# install new instance
+# install dependencies
 sudo apt update
 sudo apt install gdebi-core && sudo apt install r-base
+
+# download working install file
 wget https://s3.amazonaws.com/rstudio-ide-build/desktop/jammy/amd64/rstudio-2022.07.0-546-amd64.deb
+
+# install and delete install file
 sudo gdebi install ./rstudio-2022.07.0-546-amd64.deb && ./rstudio-2022.07.0-546-amd64.deb
+
+# install and move libraries to appropriate runtime directories
 var="sudo locate libcrypto.so.1.1 -n 1" && SSLLoc=$(eval $var) && cp $SSLLoc /usr/lib/libcrypto.so.1.1
