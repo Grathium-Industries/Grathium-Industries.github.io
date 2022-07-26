0c423bbc297801550ed52b4ee3d81255cad8ead2
Grathium <grath@duck.com>
2022-07-26 22:00:50 +1000

    consolidated script files

--- static/js/script.js
+++ static/js/script.js
@@ -33,6 +33,24 @@ $.ajax({
   }
 });
 
+// scroll to top button
+$(window).scroll(function() {
+  var height = $(window).scrollTop();
+  if (height > 100) {
+      $('#back2Top').fadeIn();
+  } else {
+      $('#back2Top').fadeOut();
+  }
+});
+
+$(document).ready(function() {
+  $("#back2Top").click(function(event) {
+      event.preventDefault();
+      $("html, body").animate({ scrollTop: 0 }, "slow");
+      return false;
+  });
+});
+
 let loop = () => {
   infinateScroll();
   setTimeout(loop, 1);
