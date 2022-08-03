bbd9a5b12940953cc03cc7c729b469c79a26a4ee
Grathium <grath@duck.com>
2022-08-03 20:50:49 +1000

    added cookie consent

---
+++ static/js/dist/global.js
@@ -0,0 +1,28 @@
+var removeCookieConsentBanner = function () {
+    var bannerObj = document.getElementById('cookie-consent');
+    if (bannerObj != null) {
+        bannerObj.remove();
+        bannerObj.innerHTML = '';
+    }
+};
+var createConsentCookie = function () {
+    // get the current date in the format "day month (in text) year"
+    var date = new Date();
+    var day = date.getDate();
+    var month = date.getMonth();
+    var year = date.getFullYear() + 1;
+    var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var dateString = day + " " + monthNames[month] + " " + year;
+    // create the cookie
+    document.cookie = "consent=1; expires=" + dateString + "; path=/";
+    removeCookieConsentBanner();
+};
+var checkConsentCookie = function () {
+    // get the cookie
+    var consentCookie = document.cookie;
+    // check if the cookie exists
+    if (consentCookie.indexOf("consent=1") > -1) {
+        removeCookieConsentBanner();
+    }
+};
+checkConsentCookie();
---
+++ static/js/global.ts
@@ -0,0 +1,34 @@
+let removeCookieConsentBanner = () => {
+    let bannerObj = document.getElementById('cookie-consent');
+    
+    if (bannerObj != null) {
+        bannerObj.remove();
+        bannerObj.innerHTML = '';
+    }
+}
+
+let createConsentCookie = () => {
+    // get the current date in the format "day month (in text) year"
+    let date = new Date();
+
+    let day = date.getDate();
+    let month = date.getMonth();
+    let year = date.getFullYear() + 1;
+    let monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let dateString = day + " " + monthNames[month] + " " + year;
+    // create the cookie
+    document.cookie = "consent=1; expires=" + dateString + "; path=/";
+
+    removeCookieConsentBanner();
+}
+
+let checkConsentCookie = () => {
+    // get the cookie
+    let consentCookie = document.cookie;
+    // check if the cookie exists
+    if (consentCookie.indexOf("consent=1") > -1) {
+        removeCookieConsentBanner();
+    }
+}
+
+checkConsentCookie();
