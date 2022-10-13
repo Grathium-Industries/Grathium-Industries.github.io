ca5cff68d68bb83cf1cd46f4b199446888ef1fda
hudson-newey <grath@duck.com>
2022-10-13 22:43:30 +1000

    updated submodules

--- update-remote.sh
+++ update-remote.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-git submodule update --remote --merge;
+remoteRepos=("NeonX2.0" "Internet-Speed-Test" "Online-Calculator");
+
+cd ./posts/deployments/;
+
+for element in "${remoteRepos[@]}"
+do
+    echo "Updating $element";
+    cd $element;
+    git checkout origin master;
+    git checkout origin main;
+    git pull;
+    cd ..;
+done
