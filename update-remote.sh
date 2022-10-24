358d0cc47be6a63bbdc5543f65bbe20262af13ea
Hudson Newey <33742269+hudson-newey@users.noreply.github.com>
2022-10-24 15:34:43 +1000

    created pwa

--- update-remote.sh
+++ update-remote.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-remoteRepos=("NeonX2.0" "Internet-Speed-Test" "Online-Calculator");
-
-cd ./posts/deployments/;
-
-for element in "${remoteRepos[@]}"
-do
-    echo "Updating $element";
-    cd $element;
-    git checkout origin master;
-    git checkout origin main;
-    git pull;
-    cd ..;
-done
+git submodule update --remote --merge;
