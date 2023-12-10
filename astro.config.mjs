e45b03e6cb4cf1bbfbdd229075525944eb1d63f1
hudson-newey <grath@duck.com>
2023-12-10 21:24:53 +1000

    Added prefetching

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,13 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astronaut.github.io",
+  site: "https://grathium-industries.github.io",
   base: "/",
+  prefetch: {
+    prefetch: true,
+    prefetchAll: true,
+    defaultStrategy: "hover",
+  },
   vite: {
     server: {
       watch: {
